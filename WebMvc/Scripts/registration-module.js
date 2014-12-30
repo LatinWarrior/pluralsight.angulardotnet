36b7b8a94e4b443c85a2a9dae012f5123b5a1232
Luis Blanco 路易斯.布兰科 <luis_blanco@volusion.com>
2014-12-30 17:45:22 -0600

    save again

--- WebMvc/Scripts/registration-module.js
+++ WebMvc/Scripts/registration-module.js
@@ -2,8 +2,7 @@
 
 var registrationModule = angular.module('registrationModule', ['ngRoute'])
     .config(function ($routeProvider, $locationProvider) {
-        console.log($routeProvider);
-    alert("Hello");
+        console.log($routeProvider);   
         $routeProvider.when('/Registration/Courses', { templateUrl: '/Templates/courses.html', controller: 'CoursesController' });
         $routeProvider.when('/Registration/Instructors', { templateUrl: '/Templates/instructors.html', controller: 'InstructorsController' });
         $locationProvider.html5Mode(true);
