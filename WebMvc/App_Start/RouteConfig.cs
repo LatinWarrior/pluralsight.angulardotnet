981daf20004e50175c71135c9b2824112d87278c
Luis Blanco 路易斯.布兰科 <luis_blanco@volusion.com>
2014-12-30 21:48:55 -0600

    fix routes

--- WebMvc/App_Start/RouteConfig.cs
+++ WebMvc/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace WebMvc
@@ -12,6 +8,18 @@ namespace WebMvc
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Registration Courses SPA",
+                url: "Registration/Courses",
+                defaults: new {controller = "Registration", action = "Index"}
+                );
+
+            routes.MapRoute(
+                name: "Registration Intructors SPA",
+                url: "Registration/Instructors",
+                defaults: new { controller = "Registration", action = "Index" }
+                );
 
             routes.MapRoute(
                 name: "Default",
